6faa0eca1691773ef7b29abf8ec6b1b9da89be6a
bandiougou <bandiougou.traore@epitech.eu>
2024-12-30 16:16:28 +0100

    vectore generated with chroma

--- routes/ingest.py
+++ routes/ingest.py
@@ -1,25 +1,16 @@
-from fastapi import APIRouter, HTTPException, File, UploadFile, Body, Depends
+from fastapi import APIRouter, HTTPException, File, UploadFile
 from typing import List
-from llama_index.core import Document, StorageContext, VectorStoreIndex
-from llama_index.vector_stores.supabase import SupabaseVectorStore
-from langchain_ollama import OllamaEmbeddings
 
-from config import Config, get_config
-import os
 from utils.DocumentExtractor import  DocumentExtractor
 from utils.app_langchain.data_parser import parse_data
 from utils.app_langchain.process_vector import process_vector
-from langchain_chroma import Chroma
 
 router = APIRouter()
 
 
 @router.post("/ingest", tags=["Documents"])
 async def process_ingest(
-    # collection_name: str,
-    # uid: str = Body(...),
     files: List[UploadFile] = File(...),
-    config: Config = Depends(get_config),
 ):
     """
     POST Endpoint to ingest multiple files
@@ -37,10 +28,6 @@ async def process_ingest(
     extractor = DocumentExtractor()
 
     try:
-        embeddings = OllamaEmbeddings(model=get_config().APP_MODEL)
-
-        if os.path.exists(config.DB_NAME):
-            Chroma(persist_directory=config.DB_NAME, embedding_function=embeddings).delete_collection()
         content = await extractor.extract_content(files)
         parsed_data = parse_data(content)
         vectorstore = await process_vector(parsed_data)
--- utils/app_langchain/process_vector.py
+++ utils/app_langchain/process_vector.py
@@ -5,10 +5,13 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 import os
-from config import Config, get_config
 
 async def process_vector(data):
         embeddings = OllamaEmbeddings(model=os.getenv("APP_MODEL"))
+
+        if os.path.exists(os.getenv("DB_NAME")):
+            Chroma(persist_directory=os.getenv("DB_NAME"), embedding_function=embeddings).delete_collection()
+
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         chunks = text_splitter.split_documents(data)
         vectorstore = Chroma.from_documents(documents=chunks, embedding=embeddings, persist_directory=os.getenv("DB_NAME"))
